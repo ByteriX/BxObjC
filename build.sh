9b2086c1852a56bd31e4b7118eb0d6ecbb33d8bd
Sergey Balalaev <sof.bix@mail.ru>
2018-01-12 15:54:43 +0400

    updated build script to 1.1 version

--- build.sh
+++ build.sh
@@ -1,9 +1,16 @@
 
+#
+#  build.sh
+#  version 1.1
+#
+#  Created by Sergey Balalaev on 02.03.17.
+#  Copyright (c) 2017 ByteriX. All rights reserved.
+#
 
-
+PROJECT_NAME="BxObjC"
 APP_CONFIG_PATH="./build.config"
-TEMPLATE_SPEC_PATH="BxObjC.templatespec"
-WORK_SPEC_PATH="BxObjC.podspec"
+TEMPLATE_SPEC_PATH="${PROJECT_NAME}.templatespec"
+WORK_SPEC_PATH="${PROJECT_NAME}.podspec"
 VAR_NAME="VERSION_NUMBER"
 
 
@@ -18,12 +25,12 @@ checkExit(){
 }
 
 tag(){
-	git tag -f -a "${VERSION_NUMBER}" -m build
-	git push -f --tags
+    git tag -f -a "${VERSION_NUMBER}" -m build
+    git push -f --tags
 }
 
 clear(){
-	rm -f -d "${WORK_SPEC_PATH}"
+    rm -f -d "${WORK_SPEC_PATH}"
     rm -f -d "${WORK_SPEC_PATH}-e"
 }
 
@@ -31,7 +38,7 @@ clear(){
 
 if [[ "$1" != "" ]]
     then
-    	CUSTOM_BUILD=1
+        CUSTOM_BUILD=1
         echo "VERSION_NUMBER=$1" > "$APP_CONFIG_PATH"
     fi
 . "$APP_CONFIG_PATH"
@@ -46,3 +53,5 @@ checkExit
 pod trunk push "${WORK_SPEC_PATH}" --allow-warnings --verbose
 checkExit
 clear
+curl "http://207.254.41.223:4567/redeploy/${PROJECT_NAME}/latest"
+
