5f684a69075b93ef5b9fa89536cd6c7f6139d6d3
Sergey Balalaev <sof.bix@mail.ru>
2023-06-27 02:46:21 +0400

    Test

---
+++ iBXCommon/Test/BxObjC.h
@@ -0,0 +1,14 @@
+/**
+ *	@file BxException.h
+ *	@namespace iBXCommon
+ *
+ *	@details Исключения
+ *	@date 29.08.2013
+ *	@author Sergey Balalaev
+ *
+ *	@version last in https://github.com/ByteriX/BxObjC
+ *	@copyright The MIT License (MIT) https://opensource.org/licenses/MIT
+ *	 Copyright (c) 2016 ByteriX. See http://byterix.com
+ */
+
+#import "BxException.h"
